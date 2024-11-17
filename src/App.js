07cdf5f96bf6794a35188fb4649d1ca0e349bd05
100ehtuzam <1995mdehtesham@gmail.com>
2024-11-18 00:37:47 +0530

    adding new features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-  return <h1> Hello</h1>
+  return(
+      <>
+      <h1> Hello</h1>
+      <p>Adding new features</p>
+      </>
+      
+    );
+
 }
 
 export default App;
